8317902b0cde116c3662ee76736e5a4325d85008
bhanu254 <48407848+bhanu254@users.noreply.github.com>
2019-11-20 15:42:00 +0530

    Add files via upload

---
+++ JavaProgram.java
@@ -0,0 +1,22 @@
+import java.util.Scanner;
+
+public class JavaProgram
+{
+    public static void main(String args[])
+    {
+        int a, b, big;
+        a = 25;
+        b = 30;
+		
+        if(a>b)
+        {
+            big = a;
+        }
+        else
+        {
+            big = b;
+        }
+		
+        System.out.print("Largest of Two Number is " +big);
+    }
+}
